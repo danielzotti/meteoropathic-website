c8d36b840dd479f95fb66c7955066784f6e588c7
Daniel Zotti <daniel.zotti87@gmail.com>
2021-05-24 09:16:37 +0200

    Add rxjs + export getVideo in FaceApiService

--- src/components/WebcamVideo.vue
+++ src/components/WebcamVideo.vue
@@ -17,47 +17,63 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import FaceApiService from '@/services/FaceApiService';
-import { detectAllFaces, TinyFaceDetectorOptions, draw } from 'face-api.js';
+import {
+  TinyFaceDetectorOptions,
+  draw,
+  WithFaceExpressions,
+  WithFaceDetection, detectSingleFace, WithFaceDescriptor, FaceDetection, WithFaceLandmarks, FaceLandmarks68
+} from 'face-api.js';
+
 
 export default defineComponent({
   name: 'WebcamVideo',
-  mounted: function() {
-    FaceApiService.init();
+  mounted: async function() {
+    await FaceApiService.init();
     const videoRef = this.$refs.video as HTMLVideoElement;
     const canvasRef = this.$refs.canvas as HTMLCanvasElement;
-    startVideo(videoRef);
+    videoRef.srcObject = await FaceApiService.getVideo();
     videoRef.addEventListener('play', function() {
       const interval = setInterval(async function() {
-        const detections = await detectAllFaces(videoRef, new TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors();
+        const detection = await detectSingleFace(videoRef, new TinyFaceDetectorOptions())
+            .withFaceLandmarks()
+            .withFaceExpressions()
+            .withFaceDescriptor();
+        console.log(detection);
+        if(detection) manageExpressions(detection);
 
-        if(detections?.length) {
+        if(detection) {
           clearCanvas(canvasRef);
-          draw.drawDetections(canvasRef, detections);
-          console.log(detections[0]);
+          draw.drawDetections(canvasRef, detection);
           //clearInterval(interval);
-          return;
         }
-      }, 100);
+      }, 250);
     });
   },
   methods: {}
 });
 
-const startVideo = (videoRef: HTMLVideoElement) => {
-  navigator.getUserMedia(
-      {
-        video: {}
-      },
-      stream => videoRef.srcObject = stream,
-      err => console.error(err)
-  );
-};
+
 
 const clearCanvas = (canvas: HTMLCanvasElement) => {
   const context = canvas.getContext('2d');
   context?.clearRect(0, 0, canvas.width, canvas.height);
-}
+};
 
+const manageExpressions = (detection: WithFaceDescriptor<WithFaceExpressions<WithFaceLandmarks<{ detection: FaceDetection; }, FaceLandmarks68>>>) => {
+  const res = Object.entries(detection.expressions).reduce((max, [key, value]) => {
+    return value > max.value ? { key, value } : max;
+  }, { key: 'none', value: 0 });
+  console.log({ expressionDetected: res });
+  //console.log(detection);
+  //if(!detection || !detection.expressions) {
+  //  console.log('no detection');
+  //  return;
+  //}
+  //const res = Object.entries(detection.expressions).map(([key, value]) => {
+  //  debugger;
+  //  return { key, value };
+  //}, { key: undefined, value: 0 } as { key: string | undefined, value: number });
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/services/FaceApiService.ts
+++ src/services/FaceApiService.ts
@@ -1,4 +1,5 @@
 import {
+  loadFaceExpressionModel,
   loadFaceLandmarkModel,
   loadFaceRecognitionModel,
   loadSsdMobilenetv1Model,
@@ -13,7 +14,24 @@ export default class FaceApiService {
     await loadSsdMobilenetv1Model(MODEL_URL);
     await loadFaceLandmarkModel(MODEL_URL);
     await loadFaceRecognitionModel(MODEL_URL);
+    await loadFaceExpressionModel(MODEL_URL);
     await loadTinyFaceDetectorModel(MODEL_URL);
+  }
+
+  static async getVideo(constraints = {
+    audio: false,
+    video: {
+      width: { max: 1280 },
+      height: { max: 720 }
+    }
+  }): Promise<MediaStream | null> {
+
+    try {
+      return await navigator.mediaDevices.getUserMedia(constraints);
+    } catch(err) {
+      console.log(err)
+    }
+    return null;
   }
 
 }
