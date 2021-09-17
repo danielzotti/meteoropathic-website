531b645d637ed5bc3ca109b8f98cb5a8cb9c216d
Daniel Zotti <daniel.zotti87@gmail.com>
2021-09-17 23:26:41 +0200

    Add docs folder to publish on github

---
+++ docs/precache-manifest.17892e9168e09a4c264fd6658715cbef.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "164687ab8c6b1a21aeca",
+    "url": "/css/app.943b3b7a.css"
+  },
+  {
+    "revision": "ca764241d65927dc4e3133a638a55520",
+    "url": "/img/meteo/cloudy.jpg"
+  },
+  {
+    "revision": "78a47f9c5fb0d54946bad412de05185c",
+    "url": "/img/meteo/pollution.jpg"
+  },
+  {
+    "revision": "603efe3f4512f056d9827aec480587b7",
+    "url": "/img/meteo/rain.jpg"
+  },
+  {
+    "revision": "c29e475670a69c5d22afd1dd5dbbee82",
+    "url": "/img/meteo/storm.jpg"
+  },
+  {
+    "revision": "271c44a3d7bc51b12c86f368a7035c5d",
+    "url": "/img/meteo/sun.jpg"
+  },
+  {
+    "revision": "b589e007f8aa6b758dec98dcf1256150",
+    "url": "/img/meteo/sunset.jpg"
+  },
+  {
+    "revision": "ccae97a03ddfab1f1d0eb7d965fd1b25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "164687ab8c6b1a21aeca",
+    "url": "/js/app.eabfa14e.js"
+  },
+  {
+    "revision": "441ff5625a48c8acca62",
+    "url": "/js/chunk-vendors.9769adf7.js"
+  },
+  {
+    "revision": "1ca12ff908bb4def543c9f12c38dc478",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "c34648b1f6dcf740eedef0473f13f4e1",
+    "url": "/models/age_gender_model-shard1"
+  },
+  {
+    "revision": "c34648b1f6dcf740eedef0473f13f4e1",
+    "url": "/models/age_gender_model-shard1 (1)"
+  },
+  {
+    "revision": "d443abfd550a910c026d40cad6ea6000",
+    "url": "/models/age_gender_model-weights_manifest.json"
+  },
+  {
+    "revision": "33ec63fec9fc41801930d44f4f4ea8f0",
+    "url": "/models/face_expression_model-shard1"
+  },
+  {
+    "revision": "1eee5a2eea5ea5652904a2af88333dc1",
+    "url": "/models/face_expression_model-weights_manifest.json"
+  },
+  {
+    "revision": "124304f06e07fcf928290ff776e96141",
+    "url": "/models/face_landmark_68_model-shard1"
+  },
+  {
+    "revision": "1d4029763003335bc6921aadeb58706a",
+    "url": "/models/face_landmark_68_model-weights_manifest.json"
+  },
+  {
+    "revision": "47047fee26557b55d985952bdfc6cba1",
+    "url": "/models/face_landmark_68_tiny_model-shard1"
+  },
+  {
+    "revision": "29ea9c5c0e59a3069f8999b4ba1bd173",
+    "url": "/models/face_landmark_68_tiny_model-weights_manifest.json"
+  },
+  {
+    "revision": "cb6f0f62e7598d70acf76483185a962b",
+    "url": "/models/face_recognition_model-shard1"
+  },
+  {
+    "revision": "f2091ed03625f6e164a637c2326691c1",
+    "url": "/models/face_recognition_model-shard2"
+  },
+  {
+    "revision": "f2091ed03625f6e164a637c2326691c1",
+    "url": "/models/face_recognition_model-shard2 (1)"
+  },
+  {
+    "revision": "6ecdaf3ea10d4fd3792e485f971e8b96",
+    "url": "/models/face_recognition_model-weights_manifest.json"
+  },
+  {
+    "revision": "846e8dd4e6a7ed12a2c749324adf0e11",
+    "url": "/models/mtcnn_model-shard1"
+  },
+  {
+    "revision": "781bd744a5399d7cb0516d2a59a7c1c0",
+    "url": "/models/mtcnn_model-weights_manifest.json"
+  },
+  {
+    "revision": "37ef238973ea93daac91f1914478c40b",
+    "url": "/models/ssd_mobilenetv1_model-shard1"
+  },
+  {
+    "revision": "b6d5e81e2506145360be5c4278067080",
+    "url": "/models/ssd_mobilenetv1_model-shard2"
+  },
+  {
+    "revision": "cd2d65ec62107ba72b8b8d5047011647",
+    "url": "/models/ssd_mobilenetv1_model-weights_manifest.json"
+  },
+  {
+    "revision": "2e48b20953b0c59df47459d0319843a0",
+    "url": "/models/tiny_face_detector_model-shard1"
+  },
+  {
+    "revision": "5bab50532388f5da9b4cd85b15adc11c",
+    "url": "/models/tiny_face_detector_model-weights_manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
